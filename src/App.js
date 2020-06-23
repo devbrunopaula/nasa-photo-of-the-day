dc77110bea36ae09efa1e01b8f8f510032c5bba5
Bruno Paula <brunopaula@dryserv.com>
2020-06-23 19:42:59 -0400

    added axios and teched Nasa Data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import "./App.css";
+import axios from "axios"
 
 function App() {
+
+  const [ data, setData ] = useState([])
+
+  const fetchNasaData =  async ()  =>   {
+    try {
+      const fetchedData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=4av57rUOmcVq1a3KgBBWoh9YEIgAN8DmqO8u7AO3')
+      const response =  fetchedData.data
+      setData(response)
+    } catch (error) {
+      console.log(error)
+    }
+ }
+
+  useEffect(() => {
+    
+    fetchNasaData()
+  },[])
+  
+
+  console.log(data)
   return (
     <div className="App">
       <p>
