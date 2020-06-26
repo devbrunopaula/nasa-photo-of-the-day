89d2d787d199ae477877d64003e89a5466642913
Bruno Paula <brunopaula@dryserv.com>
2020-06-26 18:54:11 -0400

    Updates

---
+++ precache-manifest.a694a3ead890ce957e374dc3074d0b05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987e4ad31c2053073d33071aa8b6aef6",
+    "url": "/nasa-photo-of-the-day/index.html"
+  },
+  {
+    "revision": "19d340a97330f3cd0b55",
+    "url": "/nasa-photo-of-the-day/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "e3fde23361bd8a68dd58",
+    "url": "/nasa-photo-of-the-day/static/css/main.78669f8e.chunk.css"
+  },
+  {
+    "revision": "19d340a97330f3cd0b55",
+    "url": "/nasa-photo-of-the-day/static/js/2.9984a37a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nasa-photo-of-the-day/static/js/2.9984a37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fde23361bd8a68dd58",
+    "url": "/nasa-photo-of-the-day/static/js/main.bbfed735.chunk.js"
+  },
+  {
+    "revision": "da96db902ce543166044",
+    "url": "/nasa-photo-of-the-day/static/js/runtime-main.f5d077ed.js"
+  },
+  {
+    "revision": "80115d2cb996a9496e06db2e2e81c272",
+    "url": "/nasa-photo-of-the-day/static/media/nasa-logo.80115d2c.svg"
+  }
+]);
